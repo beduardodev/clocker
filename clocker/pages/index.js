4e838d33ef83ae4e9b442d88bf722c9ae3f3cdd4
BEduardo <beduardo.dev@gmail.com>
2021-04-06 20:09:18 -0300

    Adicionando campo de usuario, e botao de confirmacao

--- clocker/pages/index.js
+++ clocker/pages/index.js
@@ -1,9 +1,47 @@
 import { Logo } from './../components'
+import {
+  Container,
+  Box,
+  Input,
+  Button,
+  Text,
+  FormControl,
+  FormLabel,
+  FormHelperText
+} from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div>
+    <Container p={4} centerContent>
       <Logo />
-    </div>
+
+      <Box p={4} mt={8}>
+        <Text>Crie sua agenda compartilhada!</Text>
+      </Box>
+
+      <Box>
+        <FormControl id="email" p={4} isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" />
+        </FormControl>
+
+        <FormControl id="password" p={4} isRequired>
+          <FormLabel>Senha</FormLabel>
+          <Input type="password" />
+        </FormControl>
+
+        <Box display="flex" flexDirection="row" alignItems="center">
+          <Text>clocker.work/</Text>
+          <FormControl id="username" p={4} isRequired>
+            <Input type="username" />
+          </FormControl>
+        </Box>
+
+        <Box p={4}>
+          <Button width="100%">Entrar</Button>
+        </Box>
+
+      </Box>
+    </Container>
   )
 }
