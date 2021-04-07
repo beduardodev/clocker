1f2e087cc676f99b39975c58e0423e63c07cf062
BEduardo <beduardo.dev@gmail.com>
2021-04-06 21:47:04 -0300

    Configuracoes estavam obsoletas

--- clocker/config/firebase/index.js
+++ clocker/config/firebase/index.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9xJuDusoSPpgSMTw1Z39tRqeXCZ3QfEg",
-    authDomain: "clocker-brn.firebaseapp.com",
-    projectId: "clocker-brn",
-    storageBucket: "clocker-brn.appspot.com",
-    messagingSenderId: "554759099966",
-    appId: "1:554759099966:web:cc323dc0c7c9981a58236f",
-    measurementId: "G-5YM08F7YYC"
+    // apiKey: "AIzaSyD9xJuDusoSPpgSMTw1Z39tRqeXCZ3QfEg",
+    // authDomain: "clocker-brn.firebaseapp.com",
+    // projectId: "clocker-brn",
+    // storageBucket: "clocker-brn.appspot.com",
+    // messagingSenderId: "554759099966",
+    // appId: "1:554759099966:web:cc323dc0c7c9981a58236f",
+    // measurementId: "G-5YM08F7YYC"
 };
 
 export default firebase.apps.length
