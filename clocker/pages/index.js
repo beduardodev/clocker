0d3bd6f6a254b8547f452dcfc9813fb6e4f2eaeb
BEduardo <beduardo.dev@gmail.com>
2021-04-06 22:07:46 -0300

    Criando pagina de signup, adicionando rotina de login e de signup.

--- clocker/pages/index.js
+++ clocker/pages/index.js
@@ -1,82 +1,82 @@
 import { useFormik } from 'formik'
 import * as yup from 'yup'
+import Link from 'next/link'
 
 import {
-  Container,
-  Box,
-  Input,
-  Button,
-  Text,
-  FormControl,
-  FormLabel,
-  FormHelperText,
-  InputLeftAddon,
-  InputGroup
+    Container,
+    Box,
+    Input,
+    Button,
+    Text,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    InputLeftAddon,
+    InputGroup
 } from '@chakra-ui/react'
 
-import { Logo } from './../components'
-import firebase from './../config/firebase'
+import { Logo } from '../components'
+import firebase from '../config/firebase'
 
 const validationSchema = yup.object().shape({
-  email: yup.string().email('Email inválido!').required('Preenchimento Obrigatório!'),
-  password: yup.string().required('Preenchimento Obrigatório!'),
-  username: yup.string().required('Preenchimento Obrigatório!')
+    email: yup.string().email('Email inválido!').required('Preenchimento Obrigatório!'),
+    password: yup.string().required('Preenchimento Obrigatório!'),
+    username: yup.string().required('Preenchimento Obrigatório!')
 })
 
 export default function Home() {
-  const { values,
-    errors,
-    touched,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    isSubmitting
-  } = useFormik({
-    onSubmit: (values, form) => {
+    const { values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+    } = useFormik({
+        onSubmit: async (values, form) => {
+            try {
+                const user = await firebase.auth().signInWithEmailAndPassword(values.email, values.password);
+                console.log(user);
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        validationSchema,
+        initialValues: {
+            email: '',
+            username: '',
+            password: ''
+        }
+    })
 
-    },
-    validationSchema,
-    initialValues: {
-      email: '',
-      username: '',
-      password: ''
-    }
-  })
+    return (
+        <Container p={4} centerContent>
+            <Logo />
 
-  return (
-    <Container p={4} centerContent>
-      <Logo />
+            <Box p={4} mt={8}>
+                <Text>Crie sua agenda compartilhada!</Text>
+            </Box>
 
-      <Box p={4} mt={8}>
-        <Text>Crie sua agenda compartilhada!</Text>
-      </Box>
+            <Box>
+                <FormControl id="email" p={4} isRequired>
+                    <FormLabel>Email</FormLabel>
+                    <Input size="lg" type="email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
+                    {touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText>}
+                </FormControl>
 
-      <Box>
-        <FormControl id="email" p={4} isRequired>
-          <FormLabel>Email</FormLabel>
-          <Input size="lg" type="email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
-          {touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText>}
-        </FormControl>
+                <FormControl id="password" p={4} isRequired>
+                    <FormLabel>Senha</FormLabel>
+                    <Input size="lg" type="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+                    {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText>}
+                </FormControl>
 
-        <FormControl id="password" p={4} isRequired>
-          <FormLabel>Senha</FormLabel>
-          <Input size="lg" type="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
-          {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText>}
-        </FormControl>
+                <Box p={4}>
+                    <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
+                </Box>
 
-        <FormControl id="username" p={4} isRequired>
-          <InputGroup size="lg" >
-            <InputLeftAddon children="clocker.work/" />
-            <Input size="lg" type="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
-          </InputGroup>
-          {touched.username && <FormHelperText textColor="#e74c3c">{errors.username}</FormHelperText>}
-        </FormControl>
+                <Link href="/signup">Ainda não tem uma conta? Clique aqui e cadastre-se!</Link>
 
-        <Box p={4}>
-          <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
-        </Box>
-
-      </Box>
-    </Container>
-  )
+            </Box>
+        </Container>
+    )
 }
---
+++ clocker/pages/signup.js
@@ -0,0 +1,87 @@
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+
+import {
+  Container,
+  Box,
+  Input,
+  Button,
+  Text,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  InputLeftAddon,
+  InputGroup
+} from '@chakra-ui/react'
+
+import { Logo } from '../components'
+import firebase from '../config/firebase'
+
+const validationSchema = yup.object().shape({
+  email: yup.string().email('Email inválido!').required('Preenchimento Obrigatório!'),
+  password: yup.string().required('Preenchimento Obrigatório!'),
+  username: yup.string().required('Preenchimento Obrigatório!')
+})
+
+export default function Home() {
+  const { values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting
+  } = useFormik({
+    onSubmit: async (values, form) => {
+      try {
+        const user = await firebase.auth().createUserWithEmailAndPassword(values.email, values.password);
+        console.log(user);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    validationSchema,
+    initialValues: {
+      email: '',
+      username: '',
+      password: ''
+    }
+  })
+
+  return (
+    <Container p={4} centerContent>
+      <Logo />
+
+      <Box p={4} mt={8}>
+        <Text>Crie sua agenda compartilhada!</Text>
+      </Box>
+
+      <Box>
+        <FormControl id="email" p={4} isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input size="lg" type="email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
+          {touched.email && <FormHelperText textColor="#e74c3c">{errors.email}</FormHelperText>}
+        </FormControl>
+
+        <FormControl id="password" p={4} isRequired>
+          <FormLabel>Senha</FormLabel>
+          <Input size="lg" type="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+          {touched.password && <FormHelperText textColor="#e74c3c">{errors.password}</FormHelperText>}
+        </FormControl>
+
+        <FormControl id="username" p={4} isRequired>
+          <InputGroup size="lg" >
+            <InputLeftAddon children="clocker.work/" />
+            <Input size="lg" type="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
+          </InputGroup>
+          {touched.username && <FormHelperText textColor="#e74c3c">{errors.username}</FormHelperText>}
+        </FormControl>
+
+        <Box p={4}>
+          <Button colorScheme="blue" width="100%" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
+        </Box>
+
+      </Box>
+    </Container>
+  )
+}
