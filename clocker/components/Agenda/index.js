231902254ec34c2405435ea35d29d24b15ba93a0
BEduardo <beduardo.dev@gmail.com>
2021-04-07 20:06:43 -0300

    Adicionando botao de sair a tela de Agenda

--- clocker/components/Agenda/index.js
+++ clocker/components/Agenda/index.js
@@ -1,7 +1,11 @@
+import { Button } from '@chakra-ui/react'
+import firebase from './../../config/firebase'
+
 export const Agenda = () => {
+    const logout = () => firebase.auth().signOut()
     return (
         <div>
-            Agenda
+            <Button onClick={logout}>Sair</Button>
         </div>
     )
 }
